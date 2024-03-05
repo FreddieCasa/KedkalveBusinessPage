a00d76eabd69108f184bb36c2aa4ad10a285b7f8
FreddieCasa <lambada.is.agurkas@gmail.com>
2024-03-05 17:05:04 +0200

    add auto slider

---
+++ javaScript.js
@@ -0,0 +1,46 @@
+function navBarFunction() {
+  var x = document.getElementById("navLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+$(document).ready(function () {
+  var images = []; // array to hold image paths
+  var currentIndex = 0; // current index of the image being displayed
+  var interval = 3000; // interval between slide changes in milliseconds
+
+  // Function to load images from a folder
+  function loadImagesFromFolder(folderPath) {
+    $.ajax({
+      url: folderPath,
+      success: function (data) {
+        $(data)
+          .find("a")
+          .attr("href", function (i, val) {
+            if (val.match(/\.(jpe?g|png|gif)$/)) {
+              images.push(val); // push image path to the array
+            }
+          });
+        showImage(currentIndex); // display the first image
+        setInterval(nextImage, interval); // set interval to switch images automatically
+      },
+    });
+  }
+
+  // Function to display an image at the specified index
+  function showImage(index) {
+    $("#slider img").attr("src", images[index]);
+  }
+
+  // Function to show the next image in the slider
+  function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length; // loop back to the beginning if at the end
+    showImage(currentIndex);
+  }
+
+  // Call the function to load images from a folder
+  loadImagesFromFolder("/images/slider");
+});
